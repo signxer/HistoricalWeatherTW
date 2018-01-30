45a8d61d1f462042872f3a5bce0b7e16efe3026d
Sign.X.er <signxer@gmail.com>
2018-01-30 15:07:31 +0800

    Update HistoricalWeatherTW.py

--- HistoricalWeatherTW.py
+++ HistoricalWeatherTW.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 #定義起始日期
 begin_date = datetime.date(2016,1,1)
-end_date = datetime.date(2016,1,1)
+end_date = datetime.date(2016,12,31)
 conv2num = True #是否需要轉成數字
 
 def GetWeather(date,station_id,station_name):
